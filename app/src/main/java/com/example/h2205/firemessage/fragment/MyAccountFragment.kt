2dc55a9f32a75acf0db6ca83091619209a1367cb
h2205 <had2205@gmail.com>
2018-10-27 18:02:37 +0300

    ini

--- app/src/main/java/com/example/h2205/firemessage/fragment/MyAccountFragment.kt
+++ app/src/main/java/com/example/h2205/firemessage/fragment/MyAccountFragment.kt
@@ -22,6 +22,7 @@ import kotlinx.android.synthetic.main.fragment_my_account.view.*
 import org.jetbrains.anko.clearTask
 import org.jetbrains.anko.newTask
 import org.jetbrains.anko.support.v4.intentFor
+import org.jetbrains.anko.support.v4.toast
 import java.io.ByteArrayOutputStream
 
 
@@ -62,6 +63,8 @@ class MyAccountFragment : Fragment() {
                 }else{
                     FirestoreUtil.updateCurrentUser( editText_name.text.toString(), editText_bio.text.toString(), null )
                 }
+
+                toast("Saving")
 
             }
 
