f5cfc06c7cd4a1889e785a3e05ad8321e885d2ee
datismyname <had2205@gmail.com>
2018-10-27 23:48:45 +0300

    5

---
+++ app/src/main/java/com/example/h2205/firemessage/recyclerview/item/PersonItem.kt
@@ -0,0 +1,26 @@
+package com.example.h2205.firemessage.recyclerview.item
+
+import android.content.Context
+import com.example.h2205.firemessage.R
+import com.example.h2205.firemessage.glide.GlideApp
+import com.example.h2205.firemessage.model.User
+import com.example.h2205.firemessage.util.StorageUtil
+import com.xwray.groupie.kotlinandroidextensions.Item
+import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import kotlinx.android.synthetic.main.item_person.*
+
+class PersonItem(val person:User, val userID: String, private val context: Context):Item() {
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.textView_name.text = person.name
+        viewHolder.textView_bio.text = person.bio
+
+        if (!person.profilePicturePath.isNullOrEmpty()){
+            GlideApp.with(context)
+                    .load(StorageUtil.pathToReference(person.profilePicturePath!!))
+                    .placeholder(R.drawable.ic_account_circle_black_24dp)
+                    .into(viewHolder.imageView_profile_picture)
+        }
+    }
+
+    override fun getLayout() = R.layout.item_person
+}
